766c21277f7a4ac31f2c3a81cb5bd2bdbf4abf2f
BestMordaEver <matvey@fedyay.net>
2020-01-16 13:09:42 +0200

    added status management command, some cleanup

--- bot.lua
+++ bot.lua
@@ -3,10 +3,8 @@ local persistence = require "../utils/persistence.lua"
 local config = require "./config.lua"
 
 local groups = persistence.load(config.savefile) or {}	-- all changes to groups should be reflected in savefile immediately
---[[ groups save data in format
-{
-"groupName" = {"channelID" = "lastmsgID"}
-}
+--[[ groups save data in following format
+{"groupName" = {"channelID" = "lastmsgID"}}
 ]]
 
 local client = discordia.Client()
@@ -25,42 +23,50 @@ local commands = {		-- commands are one-word only. you can have space between pr
 	-- meta commands
 	list = "list",		-- print out the list of registered groups and channels in them
 	help = "help",		-- print out help text
+	status = "status", 	-- set bot status
 	shutdown = "shutdown",
 	test = "test"
 }
 
+local statusResolve = function (messageString)
+	if type(messageString) ~= "string" then return nil end
+	local _,_, statusType, status = messageString:find("(%w+)%s(.*)")	-- statusType can be either number or string
+	local success = pcall(function () if not tonumber(statusType) then statusType = discordia.enums.activityType[statusType:lower()] end end)
+	return (success and {type = statusType, name = status} or nil)
+end
+
 local actions = {
 	[commands.add] = function (message)
 		message.channel:broadcastTyping()
-		local _, _, groupName, channelID = message.content:find(commands.prefix.."%s?"..commands.add.."%s(%w+)%s?(%d*)")
+		local _, _, groupName, channelID = message.content:find(commands.add.."%s(%w+)%s?(%d*)")
 		if channelID == "" then channelID = message.channel.id end	-- if channelid is not present, bot defaults to current channel, same for other commands
 		if groupName == "" then
-			message.channel:send("Can't create group with empty name")
+			message:reply("Can't create group with empty name")
 			return
 		end
 		if not groups[groupName] then groups[groupName] = {} end
 		if not client:getChannel(channelID) then		-- channel doesn't exist or bot doesn't have access to it
-			message.channel:send("Invalid channel ID")
+			message:reply("Invalid channel ID")
 			return
 		end
 		groups[groupName][channelID] = message.id
 		persistence.store(config.savefile, groups)
-		message.channel:send("Added `"..client:getChannel(channelID).name.."`to `"..groupName.."` group")
+		message:reply("Added `"..client:getChannel(channelID).name.."`to `"..groupName.."` group")
 	end,
 	
 	[commands.remove] = function (message)
 		message.channel:broadcastTyping()
-		local _, _, groupName, channelID = message.content:find(commands.prefix.."%s?"..commands.remove.."%s(%w+)%s?(%d*)")
+		local _, _, groupName, channelID = message.content:find(commands.remove.."%s(%w+)%s?(%d*)")
 		if channelID == "" then channelID = message.channel.id end
 		if groups[groupName] then
 			if client:getChannel(channelID) then
 				groups[groupName][channelID] = nil
 			else
-				message.channel:send("Invalid channel ID")
+				message:reply("Invalid channel ID")
 				return
 			end
 		else
-			message.channel:send("No group `"..groupName.."`")
+			message:reply("No group `"..groupName.."`")
 			return
 		end
 		
@@ -70,48 +76,48 @@ local actions = {
 		end
 		if empty then groups[groupName] = nil end
 		persistence.store(config.savefile, groups)
-		message.channel:send("Removed `"..client:getChannel(channelID).name.."` from `"..groupName.."` group")
+		message:reply("Removed `"..client:getChannel(channelID).name.."` from `"..groupName.."` group")
 	end,
 	
 	[commands.send] = function (message)
 		message.channel:broadcastTyping()
-		local _, _, groupName, msg = message.content:find(commands.prefix.."%s?"..commands.send.."%s(%w+)%s(.*)")
+		local _, _, groupName, msg = message.content:find(commands.send.."%s(%w+)%s(.*)")
 		if groups[groupName] then
-			message.channel:send("Sending message to `"..groupName.."` group")
+			message:reply("Sending message to `"..groupName.."` group")
 			for k, _ in pairs(groups[groupName]) do
 				groups[groupName][k] = client:getChannel(k):send(msg).id	-- this id is used for deleting and editing
 				persistence.store(config.savefile, groups)
-				message.channel:send("Sent message to `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
+				message:reply("Sent message to `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
 			end
 		else
-			message.channel:send("No group `"..groupName.."`")
+			message:reply("No group `"..groupName.."`")
 		end
 	end,
 	
 	[commands.delete] = function (message)
 		message.channel:broadcastTyping()
-		local _, _, groupName = message.content:find(commands.prefix.."%s?"..commands.delete.."%s(%w+)")
+		local _, _, groupName = message.content:find(commands.delete.."%s(%w+)")
 		if groups[groupName] then
 			for k, v in pairs(groups[groupName]) do
 				client:getChannel(k):getMessage(v):delete()
-				message.channel:send("Deleted message in `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
+				message:reply("Deleted message in `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
 			end
 		else
-			message.channel:send("No group `"..groupName.."`")
+			message:reply("No group `"..groupName.."`")
 		end
 	end,
 	
 	[commands.edit] = function (message)
 		message.channel:broadcastTyping()
-		local _, _, groupName, msg = message.content:find(commands.prefix.."%s?"..commands.edit.."%s(%w+)%s(.*)")
+		local _, _, groupName, msg = message.content:find(commands.edit.."%s(%w+)%s(.*)")
 		if groups[groupName] then
-			message.channel:send("Editing last message sent to `"..groupName.."` group")
+			message:reply("Editing last message sent to `"..groupName.."` group")
 			for k, v in pairs(groups[groupName]) do
 				client:getChannel(k):getMessage(v):setContent(msg)
-				message.channel:send("Edited last message sent to `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
+				message:reply("Edited last message sent to `"..client:getChannel(k).name.."` on server `"..client:getChannel(k).guild.name.."`")
 			end
 		else
-			message.channel:send("No group `"..groupName.."`")
+			message:reply("No group `"..groupName.."`")
 		end
 	end,
 	
@@ -128,11 +134,11 @@ local actions = {
 				msg = msg.."`"..client:getChannel(channelID).name.."` on `"..client:getChannel(channelID).guild.name.."`\n"
 			end
 		end
-		message.channel:send(msg)
+		message:reply(msg)
 	end,
 	
 	[commands.help] = function (message)
-		message.channel:send("This bot helps you to organize your newsletter channels. Use prefix '+' before your commands, like +help.\n**"..
+		message:reply("This bot helps you to organize your newsletter channels. Use prefix '+' before your commands, like +help.\n**"..
 			commands.add.."** *{group_name} [channelID]*: subscribe the *[channelID]* channel to the *{group_name}* newsletter. Defaults to current channel.\n**"..
 			commands.remove.."** *{group_name} [channelID]*: unsubscribe the *[channelID]* channel from the *{group_name}* newsletter. Defaults to current channel.\n**"..
 			commands.send.."** *{group_name} {message}*: send *{message}* to all channels, that are subscribed to *{group_name}* newsletter.\n**"..
@@ -143,24 +149,25 @@ local actions = {
 	end,
 	
 	[commands.update] = function (message)
+		groups = persistence.load(config.savefile)
 		for name, group in pairs(groups) do
 			for channelID, _ in pairs(group) do
 				if not client:getChannel(channelID) then 
 					group[channelID] = nil
-					message.channel:send("Deleted `"..channelID.."` from `"..name.."` group")
+					message:reply("Deleted `"..channelID.."` from `"..name.."` group")
 				end
 			end
 		end
 		persistence.store(config.savefile, groups)
 	end,
 	
-	[commands.shutdown] = function (message)
-		message.channel:send("Shutting down gracefully\nAm no longer alive")
-		client:stop()
+	[commands.status] = function (message)
+		client:setGame(statusResolve(select(3, message.content:find(commands.status.."%s(.*)"))))
 	end,
 	
-	[commands.test] = function (message)	-- does nothing, but this can be changed (づ｡◕__◕｡)づ
-		
+	[commands.shutdown] = function (message)
+		message:reply("Shutting down gracefully\nAm no longer alive")
+		client:stop()
 	end
 }
 
@@ -168,11 +175,11 @@ client:on('messageCreate', function(message)
 	if message.author.id ~= "272093076778909707" and message.author.id ~= "188731184501620736" then return end	-- Only Bor is valid...
 	local _, _, command = message.content:find(commands.prefix.."%s?(%a+).*")
 	local res, msg = pcall(function() if actions[command] then actions[command](message) end end)
-	if not res then message.channel:send("Something went wrong, outputting error message...\n"..msg) end
+	if not res then message:reply("Something went wrong, outputting error message...\n"..msg) end
 end)
 
 client:on('ready', function()
-	client:getUser("272093076778909707"):getPrivateChannel():send("It's alive!")
+	--client:getUser("188731184501620736"):getPrivateChannel():send("It's alive!")
 end)
 
 client:run('Bot '..config.token)
